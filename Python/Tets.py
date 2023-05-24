87388e36d7cbddb9cd75164ff1bda6a3ccb8920c
S3nki Vils <seb.villalta@hotmail.com>
2023-05-24 05:45:21 +0000

    Changes to be committed: 	new file:   Python/Tets.py

---
+++ Python/Tets.py
@@ -0,0 +1,17 @@
+while True:
+    print("What is your name?")
+    name = input()
+
+    print("What is your age?")
+    age = input()
+
+    print("So you are:", name, "and your age is:", age)
+    print("Is that correct?(y/n)")
+    answer_1 = input()
+
+    if answer_1.lower() == "y":
+        print("Welcome", name)
+        break
+    else:
+        print("Please introduce your corrected Name and Age")
+        
