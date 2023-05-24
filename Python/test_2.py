8d7c9b56b74a9ab4304d217ae622ce5ecf7bb6a3
S3nki Vils <seb.villalta@hotmail.com>
2023-05-24 06:17:05 +0000

    new file:   Python/test_2.py

---
+++ Python/test_2.py
@@ -0,0 +1,22 @@
+#this is test so i can text the capabilities of gitpod
+
+import time
+
+print("Welcome to the Awesome Restaurant!")
+
+time.sleep(1)  # Pause for 1 second
+
+print("What can I get for you today?")
+
+order = input()  # Get user input for the order
+
+time.sleep(2)  # Pause for 2 seconds
+
+print(f"Sure, we'll prepare {order} for you!")
+
+time.sleep(1)  # Pause for 1 second
+
+print("Your order will be ready shortly. Enjoy your meal!")
+
+#end comment
+
